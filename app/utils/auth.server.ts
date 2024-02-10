7d4575de61aabbc54c47cb40ca29aadc6c84688f
Adrianna V. <adrianna.valdivia@salesloft.com>
2024-02-09 23:12:07 -0500

    modify existing authentication functions.

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,7 +1,35 @@
-import { json } from '@remix-run/node';
-import type { RegisterForm } from './types.server';
+import { json, redirect, createCookieSessionStorage } from '@remix-run/node';
+import bcrypt from 'bcryptjs';
+import type { RegisterForm, LoginForm } from './types.server';
 import { prisma } from './prisma.server';
 import { createUser } from './user.server';
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET must be set');
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: 'kudos-session',
+    secure: process.env.NODE_ENV === 'production',
+    secrets: [sessionSecret],
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set('userId', userId);
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  });
+}
 
 export async function register(user: RegisterForm) {
   const exists = await prisma.user.count({ where: { email: user.email } });
@@ -22,4 +50,17 @@ export async function register(user: RegisterForm) {
       { status: 400 }
     );
   }
+
+  return createUserSession(newUser.id, '/');
+}
+
+export async function login({ email, password }: LoginForm) {
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user || !(await bcrypt.compare(password, user.password)))
+    return json({ error: `Incorrect login` }, { status: 400 });
+
+  return createUserSession(user.id, '/');
 }
