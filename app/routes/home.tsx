cd320eb8931a97be53d5c716c11b0bcfaf5fd952
Adrianna V. <adrianna.valdivia@salesloft.com>
2024-02-14 23:51:41 -0500

    filter kudo by current user signed in for their home feed.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,25 +1,45 @@
 import { json, LoaderFunction } from '@remix-run/node';
 import { useLoaderData, Outlet } from '@remix-run/react';
+import { Kudo as IKudo, Profile } from '@prisma/client';
 import { requireUserId } from '~/utils/auth.server';
 import { Layout } from '~/components/Layout';
 import { UserPanel } from '~/components/UserPanel';
+import { Kudo } from '~/components/Kudo';
 import { getOtherUsers } from '~/utils/user.server';
+import { getFilteredKudos } from '~/utils/kudos.server';
+
+interface KudoWithProfile extends IKudo {
+  author: {
+    profile: Profile;
+  };
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUserId(request);
   const userId = await requireUserId(request);
   const users = await getOtherUsers(userId);
-  console.log('users', users);
-  return json({ users });
+  const kudos = await getFilteredKudos(userId, {}, {});
+  return json({ users, kudos });
 };
 
 export default function Home() {
-  const { users } = useLoaderData<typeof loader>();
+  const { users, kudos } = useLoaderData<typeof loader>();
   return (
     <Layout>
       <Outlet />
       <div className='h-full flex'>
         <UserPanel users={users} />
+        <div className='flex-1 flex flex-col'>
+          {/* Search Bar Goes Here */}
+          <div className='flex-1 flex'>
+            <div className='w-full p-10 flex flex-col gap-y-4'>
+              {kudos.map((kudo: KudoWithProfile) => (
+                <Kudo key={kudo.id} kudo={kudo} profile={kudo.author.profile} />
+              ))}
+            </div>
+            {/* Recent Kudos Goes Here */}
+          </div>
+        </div>
       </div>
     </Layout>
   );
